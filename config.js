5454709e89091be4f3f0d515c6415db2928abef3
eko-develops <ekodevelops@gmail.com>
2022-09-02 15:13:31 -0700

    add dev mode flag

--- config.js
+++ config.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 
+const devMode = true;
+
 const config = {
-	token: process.env.BOT_TOKEN,
-	client: process.env.BOT_CLIENT_ID,
+	token: devMode ? process.env.BOT_TOKEN : process.env.BOT_TOKEN_PROD,
+	client: devMode ? process.env.BOT_CLIENT_ID : process.env.BOT_CLIENT_ID_PROD,
 	companyName: process.env.COMPANY_NAME,
-	guildId: process.env.GUILD_ID,
+	guildId: devMode ? process.env.GUILD_ID : process.env.GUILD_ID_PROD,
 	adminUser: process.env.ADMIN_USER,
 	customerSupportUser: process.env.CUSTOMER_SUPPORT_USER,
 	mongodbSrv: process.env.MONGO_SRV,
