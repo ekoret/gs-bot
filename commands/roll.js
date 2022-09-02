3caaf6c0e0ed5c1bc245f243917505852daa6a1f
eko-develops <ekodevelops@gmail.com>
2022-09-01 23:06:40 -0700

    update user with new reward

--- commands/roll.js
+++ commands/roll.js
@@ -1,16 +1,25 @@
-const createUser = require('../controllers/modelController');
+const { findUser, rewardUser } = require('../controllers/userController');
 const { SlashCommandBuilder } = require('../DiscordHelper');
-const createEmbed = require('../EmbedHelper');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('roll')
 		.setDescription('Get a chance to receive free credits!'),
 	async execute(interaction) {
-		// const embed = createEmbed('Congratulations!', `You just rolled a`);
+		const id = interaction.user.id;
 
-		createUser(interaction.user.id, interaction.user.username, 10);
+		const user = await findUser(id);
 
-		await interaction.reply('New user added to db');
+		if (user === null) {
+			//There was no user found, we'll need to create one.
+			await interaction.reply('');
+		} else if (false) {
+			//We need to check if the user is timed-out.
+		} else {
+			//There was a user found, so we can update the user.
+			const { updatedUser, reward } = await rewardUser(user);
+			console.log({ updatedUser, reward });
+			await interaction.reply('credits added');
+		}
 	},
 };
---
+++ controllers/userController.js
@@ -0,0 +1,47 @@
+const User = require('../models/user');
+
+const createUser = async (messageAuthorId, messageAuthorUsername, reward) => {
+	const newUser = new User({
+		_id: messageAuthorId,
+		username: messageAuthorUsername,
+		weekly: Date.now(),
+		totalCredits: reward,
+	});
+
+	await newUser
+		.save()
+		.then((res) => {
+			console.log(
+				`User ${messageAuthorId}, ${messageAuthorUsername} has been added to the weekly lottery!`
+			);
+		})
+		.catch((err) => console.log('Error has occured creating user =>', err));
+};
+
+const findUser = async (id) => {
+	const user = await User.findById(id);
+
+	return user;
+};
+
+const rewardUser = async (user) => {
+	const reward = getRandomReward();
+	const now = Date.now();
+
+	const updatedUser = await User.findOneAndUpdate(
+		{ _id: user._id },
+		{ totalCredits: user.totalCredits + reward, weekly: now },
+		{ new: true }
+	);
+
+	return { updatedUser, reward };
+};
+
+const getRandomReward = () => {
+	const rewards = [25, 15, 15, 15, 10, 10, 10, 10, 10, 5, 5, 5, 5, 5, 5, 5, 5];
+	const randomNumber = Math.floor(Math.random() * rewards.length);
+	const reward = rewards[randomNumber];
+	return reward;
+};
+
+module.exports = { createUser, findUser, rewardUser };
