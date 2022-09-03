f9b42772dee845882747ad11eb59df1cb550ba53
eko-develops <ekodevelops@gmail.com>
2022-09-02 21:47:24 -0700

    add options for admin credits command

--- commands/credits.js
+++ commands/credits.js
@@ -7,14 +7,41 @@ const EmbedHelper = require('../helpers/EmbedHelper');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('credits')
-		.setDescription('Add or subtract credits. Admins only.')
+		.setDescription('Add or minus credits from users. Admins only.')
+		.addStringOption((option) =>
+			option
+				.setName('method')
+				.setDescription('Add or minus from users credits')
+				.setRequired(true)
+				.addChoices(
+					{ name: 'Add', value: 'add' },
+					{ name: 'Minus', value: 'minus' }
+				)
+		)
+		.addStringOption((option) =>
+			option
+				.setName('amount')
+				.setDescription('The amount of credits to add or minus')
+				.setRequired(true)
+		)
+		.addUserOption((option) =>
+			option
+				.setName('target')
+				.setDescription('The target user')
+				.setRequired(true)
+		)
 		.setDefaultMemberPermissions(
 			PermissionFlagsBits.KickMembers | PermissionFlagsBits.BanMembers
 		),
-
 	async execute(interaction) {
-		const embed = EmbedHelper.createEmbed('Admin Only Command', 'Hello');
+		const embed = EmbedHelper.createEmbed(
+			'Credits Manager',
+			'Add or subtract credits from users.'
+		);
 
-		await interaction.reply({ embeds: [embed] });
+		await interaction.reply({
+			embeds: [embed],
+			ephemeral: true,
+		});
 	},
 };
